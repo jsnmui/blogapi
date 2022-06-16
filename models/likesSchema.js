ca66ea3a4e408226bf6b8acb4cef09ad30a5a632
unknown <jasonmui2004@gmail.com>
2022-06-16 12:43:28 -0400

    7th commit

--- models/likesSchema.js
+++ models/likesSchema.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 
 const likesSchema = mongoose.Schema({
     blog_entry_id: {
-        type: mongoose.Schema.Types.ObjectId,    // links the comment to the blog
+        type: mongoose.Schema.Types.ObjectId,    // links the like to the blog
         ref: 'blog',                              // refers to blog model in schema
         required: true 
     },
     created_by: {
-        type: mongoose.Schema.Types.ObjectId,    // links the comment to user
+        type: mongoose.Schema.Types.ObjectId,    // links the like to user
         ref: 'user'                              // refers to the user model
     },
 
