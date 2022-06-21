d99b44453056685437d38d13e6f82f2b76697dc9
unknown <jasonmui2004@gmail.com>
2022-06-21 10:04:17 -0400

    new

--- routes/apidocs.js
+++ routes/apidocs.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../docs/basicInfo');
-
+// display basic info about project
 router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+router.get('/', swaggerUi.setup(swaggerDocument));  // endpoint for display in web browser
+router.get('/swagger.json', (request, response) => response.json(swaggerDocument))  // endpoint for Postman. Response is JSON object
 module.exports = router
